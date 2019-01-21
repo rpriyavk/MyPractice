0dcafc854847dce525c2a6e14a34e6f84693ef4e
Raja Priya V K <42032905+rpriyavk@users.noreply.github.com>
2019-01-21 16:15:59 +0530

    Update sample.c

--- 2DArraySorting/sample.c
+++ 2DArraySorting/sample.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void simple_sort(int *arr, int arr_size)
+/*void simple_sort(int *arr, int arr_size)
 {
+    //Insertion Sort
     int i, j;
     int tmp;
     for (i = 0; i < arr_size; i++) {
@@ -14,13 +15,13 @@ void simple_sort(int *arr, int arr_size)
             }
         }
     }
-}
+}*/
 
 int main() {
     int num_of_studs;
-    int *arr = NULL;
+    int arr[1000001];
     int f_idx, m_idx;
-    int i;
+    int i, tmp, j;
     int fm, level;
 
     scanf("%d", &num_of_studs);
@@ -28,7 +29,10 @@ int main() {
         goto err;
     }
 
-    arr = malloc(sizeof(int *) * num_of_studs);
+    /*if (num_of_studs > 100000) {
+        num_of_studs = 100000;
+    }*/
+    //arr = malloc(sizeof(int *) * num_of_studs);
     if (!arr) {
         goto err;
     }
@@ -40,19 +44,37 @@ int main() {
         if (!fm) {
             /* If female */
             arr[f_idx] = level;
+            if(f_idx > 0){
+                for(j = 0; j < f_idx; j++){
+                    if (arr[j] < arr[f_idx]) {
+                        tmp = arr[j];
+                        arr[j] = arr[f_idx];
+                        arr[f_idx] = tmp;
+                    }
+                }
+            }
             f_idx++;
         } else {
             /* If male */
             arr[m_idx] = level;
+            if(m_idx < (num_of_studs-2)){
+                for(j = num_of_studs-1; j > m_idx; j--){
+                    if (arr[j] > arr[m_idx]) {
+                        tmp = arr[j];
+                        arr[j] = arr[m_idx];
+                        arr[m_idx] = tmp;
+                    }
+                }
+            }
             m_idx--;
         }
     }
     printf("Received %d female entries\n", f_idx);
     printf("Received %d male entries\n", (num_of_studs - 1) - m_idx);
-    simple_sort(arr, f_idx);
-    simple_sort(arr + m_idx + 1, (num_of_studs - 1) - m_idx);
+    //simple_sort(arr, f_idx);
+    //simple_sort(arr + m_idx + 1, (num_of_studs - 1) - m_idx);
     for (i = 0; i < num_of_studs; i++) {
-        printf("%d\n", arr[i]);
+        printf("%d ", arr[i]);
     }
 
     return 0;
